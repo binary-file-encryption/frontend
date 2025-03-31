26bd728d0b1b9eb28a0af04747c82bb9ec98c1b3
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-04-01 02:46:33 +0900

    ✏️ [fix] 로그인 되어있는 경우에 한하여 메인 페이지 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './css/App.css'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import SingIn from './pages/SignIn'
 import SingUp from './pages/SignUp'
 import MainPage from './pages/MainPage'
 import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   // 1. "/" : 파일 조회 페이지(로그인 되어지있지 않다면 로그인 페이지로 라우팅)
@@ -11,9 +12,13 @@ function App() {
   // 3. "/signup" : 회원가입 페이지
   return (
     <Routes>
-      <Route path='/' element = {<MainPage/>}></Route>
-      <Route path='/signin' element = {<SingIn/>}></Route>
-      <Route path='/signup' element = {<SingUp/>}></Route>
+      <Route path="/" element={
+          <ProtectedRoute>
+            <MainPage />
+          </ProtectedRoute>
+        }/>
+      <Route path='/signin' element={<SingIn />}></Route>
+      <Route path='/signup' element={<SingUp />}></Route>
       <Route path='*' element={<NotFound />}></Route>
     </Routes>
   )
