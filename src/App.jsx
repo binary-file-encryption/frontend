3dfbca40a9ce6bd93faf3cbe8c955d47f47a882d
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-04-01 00:31:01 +0900

    🎨 [add] 로그인 페이지 UI 개발

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './css/App.css'
 import { Routes,Route } from 'react-router-dom'
 import SingIn from './pages/SignIn'
 import SingUp from './pages/SignUp'
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,36 @@
-const SingIn = () => {
-    return (
-        <h1>로그인 페이지</h1>
-    )
-}
+import "./../css/SignIn.css"; // CSS 파일 임포트
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 
-export default SingIn
+const SignIn = () => {
+    const nav = useNavigate()
+    return (
+        <>
+            <div className="signin-container">
+                <div className="signin-box">
+                    <h2 className="signin-title">로그인</h2>
+                    <form onSubmit={() => console.log("제출")} className="signin-form">
+                        <div className="form-group">
+                            <label>아이디</label>
+                            <input type="username" name="email" />
+                        </div>
+                        <div className="form-group">
+                            <label>비밀번호</label>
+                            <input type="password" name="password" />
+                        </div>
+                        <button type="submit" className="signin-button">
+                            로그인
+                        </button>
+                    </form>
+                    <div className="signup-redirect">
+                        계정이 없으신가요?{" "}
+                        <span className="signup-link" onClick={() => nav("/signup")}>
+                            회원가입
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SignIn;
