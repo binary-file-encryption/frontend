b37b07376c99931ef159fb6e594544275c4c79bb
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-04-01 00:44:32 +0900

    🎨 [improve] 아이디, 비밀번호 입력하지 않을시 버튼 클릭 불가

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import "./../css/SignIn.css"; // CSS 파일 임포트
-import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
     const nav = useNavigate()
+    const [signInForm, setSignInForm] = useState({ username: "", password: "" })
+
+    // 아이디, 비밀번호 입력시 동작
+    const handleChange = (e) => {
+        console.log(e.target.name)
+        console.log(e.target.value)
+        setSignInForm({ ...signInForm, [e.target.name]: e.target.value })
+    }
+
+    // 값을 모두 입력했는지 확인
+    const isFormValid = signInForm.username !== "" && signInForm.password !== "";
+
     return (
         <>
             <div className="signin-container">
@@ -10,14 +23,21 @@ const SignIn = () => {
                     <h2 className="signin-title">로그인</h2>
                     <form onSubmit={() => console.log("제출")} className="signin-form">
                         <div className="form-group">
-                            <label>아이디</label>
-                            <input type="username" name="email" />
+                            <input
+                                placeholder="아이디"
+                                name="username"
+                                value={signInForm.username}
+                                onChange={handleChange} />
                         </div>
                         <div className="form-group">
-                            <label>비밀번호</label>
-                            <input type="password" name="password" />
+                            <input
+                                placeholder="비밀번호"
+                                type="password"
+                                name="password"
+                                value={signInForm.password}
+                                onChange={handleChange} />
                         </div>
-                        <button type="submit" className="signin-button">
+                        <button type="submit" className="signin-button" disabled = {!isFormValid}>
                             로그인
                         </button>
                     </form>
