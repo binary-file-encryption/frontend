b13384dd0cce38b286e89ff8ef095fe41406904c
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2025-04-01 00:12:17 +0900

    🎨 [add] 라우팅 구조 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import SingIn from './pages/SignIn'
+import SingUp from './pages/SignUp'
+import MyFiles from './pages/MyFiles'
+import NotFound from './pages/NotFound'
 
 function App() {
-
+  // 1. "/" : 파일 조회 페이지(로그인 되어지있지 않다면 로그인 페이지로 라우팅)
+  // 2. "/signin" : 로그인 페이지
+  // 3. "/signup" : 회원가입 페이지
   return (
-    <>
-    </>
+    <Routes>
+      <Route path='/' element = {<MyFiles/>}></Route>
+      <Route path='/signin' element = {<SingIn/>}></Route>
+      <Route path='/signup' element = {<SingUp/>}></Route>
+      <Route path='*' element={<NotFound />}></Route>
+    </Routes>
   )
 }
 
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,7 @@
+const SingIn = () => {
+    return (
+        <h1>로그인 페이지</h1>
+    )
+}
+
+export default SingIn
